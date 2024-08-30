3d8c01bcd218cf2189b73c6278b527c40738d9c6
kirillgladkov <crosfire2011@gmail.com>
2024-08-30 06:38:50 +0300

    chore: Update Apple Sign In button implementation

--- lib/components/apple_sign_in_button.dart
+++ lib/components/apple_sign_in_button.dart
@@ -2,7 +2,6 @@ import 'package:deplan_subscriptions_client/constants/routes.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class AppleSignInButton extends StatelessWidget {
   const AppleSignInButton({super.key});
@@ -10,22 +9,30 @@ class AppleSignInButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Future<UserCredential> signInWithApple() async {
-      final appleProvider = AppleAuthProvider();
-      appleProvider.addScope('email');
-      appleProvider.addScope('name');
-      print(appleProvider.scopes);
+      final appleProvider = OAuthProvider("apple.com")
+        ..addScope('email')
+        ..addScope('name');
+
       if (kIsWeb) {
+        print('web');
         return await FirebaseAuth.instance.signInWithPopup(appleProvider);
       } else {
+        print('mobile');
         return await FirebaseAuth.instance.signInWithProvider(appleProvider);
       }
     }
 
-    return SignInWithAppleButton(
+    return ElevatedButton.icon(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.all<Color?>(Colors.black),
+      ),
       onPressed: () async {
         try {
           final credentials = await signInWithApple();
-          print(credentials.toString());
+          print({
+            'user': credentials.user,
+            'additionalUserInfo': credentials.additionalUserInfo,
+          });
           // go to subscriptions home route
           Navigator.pushNamed(context, Routes.subscriptionsHome);
         } on FirebaseAuthException catch (e) {
@@ -33,6 +40,8 @@ class AppleSignInButton extends StatelessWidget {
           print(e.message);
         }
       },
+      label: const Text('Sign In with Apple'),
+      icon: const Icon(Icons.apple),
     );
   }
 }
